688b6fdf7942ec8aa46751afd2264f8a56deb995
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-19 19:09:49 +0530

    Add the getter and setter methods for the private fields.

--- src/data/City.java
+++ src/data/City.java
@@ -16,5 +16,27 @@ public class City {
         this.kilometersRequired = kilometersRequired;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isTraversed() {
+        return isTraversed;
+    }
+
+    public void setTraversed(boolean traversed) {
+        isTraversed = traversed;
+    }
+
+    public int getKilometersRequired() {
+        return kilometersRequired;
+    }
+
+    public void setKilometersRequired(int kilometersRequired) {
+        this.kilometersRequired = kilometersRequired;
+    }
 }
