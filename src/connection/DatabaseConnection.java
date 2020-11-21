1a1462d1e38b4928975d70f7369e040cff97b64f
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-21 16:03:50 +0530

    Update the URL of the Database.

--- src/connection/DatabaseConnection.java
+++ src/connection/DatabaseConnection.java
@@ -16,7 +16,7 @@ public class DatabaseConnection {
     // 1. URL of the database.
     // localhost (on our computer)
     // IP --> 127.0.0.1 or domain name --> localhost
-    private static final String URL = "jdbc:mysql://localhost:3306/demo_database";
+    private static final String URL = "jdbc:mysql://localhost:3306/google_pay_game";
 
     // 2. Username of the admin of the database.
     private static final String USERNAME = "root";
