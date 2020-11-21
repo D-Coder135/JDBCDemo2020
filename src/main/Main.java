947e4ddc9aaebaed3ad1902851812db185750b55
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-21 22:46:52 +0530

    Removed some sample part of the code.

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,28 +6,11 @@
 package main;
 
 import connection.DatabaseConnection;
-import data.City;
-import helper.CityEntryHelper;
-
-import java.sql.SQLException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         DatabaseConnection connection = new DatabaseConnection();
         connection.connectToDatabase();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the name of the city you want to add: ");
-        String name = scanner.nextLine().trim();
-        System.out.println("Please tell me if you have been to the city? (true/false): ");
-        boolean isTraversed = scanner.nextBoolean();
-        int kmsRequired = (int) ((Math.random() * 2000.0) + 100.0);
-        City city = new City(name, isTraversed, kmsRequired);
-        CityEntryHelper helper = new CityEntryHelper();
-        try {
-            helper.createNewCity(DatabaseConnection.getConnection(), city);
-        } catch (SQLException exception) {
-            System.err.println(city.getName() + "could not be added to the database.");
-        }
+
     }
 }
