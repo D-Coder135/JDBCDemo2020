f81658f98285455c003641a6d5fb52ffc1139d80
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-21 22:50:37 +0530

    Add different cases along with break

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,7 +22,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
         switch (choice) {
-
+            case 1:
+                break;
+            case 2 :
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
         }
     }
 }
