771b4bc4ae8dedd415a5762e4294a6b26a8b6a05
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-21 22:11:03 +0530

    Add method signature for the first method.

--- src/helper/CityEntryHelper.java
+++ src/helper/CityEntryHelper.java
@@ -9,6 +9,9 @@ public class CityEntryHelper {
     // Inside this CityEntryHelper class, we will create methods to execute the queries.
 
     // 1. to add a city in the database (CREATE)
+    public boolean createNewCity() {
+        return false;
+    }
     // 2. to read all the cities from the database.(READ)
     // 3. to modify the attributes of a city in the database.(UPDATE)
     // 4. to delete a city from the database.(DELETE)
