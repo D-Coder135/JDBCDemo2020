4881651ff6bbc03278ee2a599ca1a0b4df669993
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-22 21:35:46 +0530

    Add throws to the method signature to handle the exceptions

--- src/helper/CityEntryHelper.java
+++ src/helper/CityEntryHelper.java
@@ -54,7 +54,7 @@ public class CityEntryHelper {
     }
 
     // 3. to modify the attributes of a city in the database.(UPDATE)
-    public void modifyCity(Connection connection) {
+    public void modifyCity(Connection connection) throws SQLException {
         PreparedStatement updateStatement = connection.prepareStatement(UPDATE_QUERY);
     }
 
