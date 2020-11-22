e9d0358576349a6eede318124776ec98277e6ac8
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-22 19:59:08 +0530

    handle the exception thrown

--- src/helper/CityEntryHelper.java
+++ src/helper/CityEntryHelper.java
@@ -33,7 +33,7 @@ public class CityEntryHelper {
     }
 
     // 2. to read all the cities from the database.(READ)
-    public void readAllCities(Connection connection) {
+    public void readAllCities(Connection connection) throws SQLException {
         PreparedStatement readStatement = connection.prepareStatement(READ_QUERY);
 
     }
