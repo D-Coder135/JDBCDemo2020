db3af53a2cfa15c91f2d23c7c107ea4e8215f33d
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-22 22:00:14 +0530

    Pass a field of String type as the parameter to the method deleteCity()

--- src/helper/CityEntryHelper.java
+++ src/helper/CityEntryHelper.java
@@ -60,7 +60,7 @@ public class CityEntryHelper {
     }
 
     // 4. to delete a city from the database.(DELETE)
-    public void deleteCity(Connection connection) throws SQLException {
+    public void deleteCity(Connection connection, String cityName) throws SQLException {
         PreparedStatement deleteStatement = connection.prepareStatement(DELETE_QUERY);
     }
 
